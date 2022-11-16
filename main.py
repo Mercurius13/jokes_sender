7cc2e4a7b832801ffe102623939ebe22b7b0079a
Jason Dsouza <76958526+Mercurius13@users.noreply.github.com>
2022-11-16 19:52:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from trycourier import Courier
+import random
+import json
+
+client = Courier(auth_token="dk_prod_8ERJE7RPPSMQXSKP53CNKQCV7MYG")
+
+with open("text_files/jokes.json", "r", encoding="utf-8") as f:
+    jokes_json = json.load(f)
+
+
+def query_data_and_send_emails(data):
+    data = jokes_json[random.randint(1, 387)]
+
+    resp = client.send_message(
+        message={
+            "brand_id": "N70YE374RY4TTXH1J7K6NRXH39GT",
+            "template": "W3ZPE5X6F6MVP4M9N6WSKD22JYAN   ",
+            "to": {
+                "email": "meet.siddhant@gmail.com",
+            },
+            "data": {
+                "recipientName": "Siddhant Mohile",
+                "joke": f"{data['setup']} {data['punchline']}",
+            },
+        }
+    )
+    print(resp)
+
+
+query_data_and_send_emails(data=jokes_json)
